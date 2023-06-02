3af5b93348f4c07e4a9213c08e26495f187c994a
lucavini <lucass.lv.lv@gmail.com>
2023-06-02 09:00:33 -0300

    feat: wrapper routes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import GlobalStyles from './Styles/global';
 import { ThemeProvider } from 'styled-components';
 import styledTheme from '~Styles/Themes/defaults';
-import Router from './router';
+import Router from './Routes/router';
 
 function App() {
   return (
