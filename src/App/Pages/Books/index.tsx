fd6b53fee88cac2f14b9450a7a69ed5986711685
lucavini <lucass.lv.lv@gmail.com>
2023-06-04 15:36:52 -0300

    feat: page icon

--- src/App/Pages/Books/index.tsx
+++ src/App/Pages/Books/index.tsx
@@ -1,8 +1,10 @@
 import Template from '@Components/template';
+import { ReactComponent as Book } from '@Assets/icons/book.svg';
 
 function Books() {
   return (
     <Template
+      Icon={Book}
       title="Livros"
       subtitle="Gerencie os livros do seu estabelecimento"
     >
