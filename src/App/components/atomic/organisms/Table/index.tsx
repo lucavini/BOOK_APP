7fc27d49472d248b2a7bf63b86ff4fd56a7b2149
lucavini <lucass.lv.lv@gmail.com>
2023-06-05 10:51:57 -0300

    feat: table rows props

--- src/App/components/atomic/organisms/Table/index.tsx
+++ src/App/components/atomic/organisms/Table/index.tsx
@@ -8,59 +8,43 @@ type IActionButtonsProps = {
 
 type Iprops = {
   actionButtons?: IActionButtonsProps[];
+  headerData: string[];
+  data: models.BooksSaled[];
 };
 
-function Table({ actionButtons }: Iprops) {
+function Table({ actionButtons, headerData, data }: Iprops) {
   return (
     <TableComponent>
       <Header>
         <Row>
-          <th>ID</th>
-          <th>Data</th>
-          <th>Título</th>
-          <th>Gênero</th>
-          <th>Preço</th>
-          <th>Ações</th>
+          {headerData.map((header) => (
+            <th key={Math.random()}>{header}</th>
+          ))}
         </Row>
       </Header>
 
       <Body>
-        <Row>
-          <td>
-            <Text size="small" font="regular">
-              123
-            </Text>
-          </td>
-          <td>
-            <Text size="small" font="regular">
-              07/12/2022
-            </Text>
-          </td>
-          <td>
-            <Text size="small" font="regular">
-              A menina que roubava livros
-            </Text>
-          </td>
-          <td>
-            <Text size="small" font="regular">
-              Romance, Historico, Aventura
-            </Text>
-          </td>
-          <td>
-            <Text size="small" font="regular">
-              R$ 40,00
-            </Text>
-          </td>
-          <td>
-            <div className='action-buttoms'>
-              {actionButtons?.map((buttom) => (
-                <div key={Math.random()} onClick={buttom.onClick}>
-                  <img src={buttom.icon} alt="icon" />
-                </div>
-              ))}
-            </div>
-          </td>
-        </Row>
+        {data.map((item) => (
+          <Row key={item.id}>
+            {Object.entries(item).map(([key, value]) => (
+              <td key={key}>
+                <Text size="small" font="regular">
+                  {value}
+                </Text>
+              </td>
+            ))}
+
+            <td>
+              <div className="action-buttoms">
+                {actionButtons?.map((buttom) => (
+                  <div key={Math.random()} onClick={buttom.onClick}>
+                    <img src={buttom.icon} alt="icon" />
+                  </div>
+                ))}
+              </div>
+            </td>
+          </Row>
+        ))}
       </Body>
     </TableComponent>
   );
